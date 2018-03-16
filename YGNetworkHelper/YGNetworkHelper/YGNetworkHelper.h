1287f2e759b0155355f3193a46ba10a0d84e1150
xuyaguang <xu_yaguang@163.com>
2018-03-16 16:40:40 +0800

    1.添加依赖库 2.创建文件

---
+++ YGNetworkHelper/YGNetworkHelper/YGNetworkHelper.h
@@ -0,0 +1,13 @@
+//
+//  YGNetworkHelper.h
+//  YGNetworkHelper
+//
+//  Created by Kevin on 2018/3/16.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YGNetworkHelper : NSObject
+
+@end
---
+++ YGNetworkHelper/YGNetworkHelper/YGNetworkHelper.m
@@ -0,0 +1,13 @@
+//
+//  YGNetworkHelper.m
+//  YGNetworkHelper
+//
+//  Created by Kevin on 2018/3/16.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import "YGNetworkHelper.h"
+
+@implementation YGNetworkHelper
+
+@end
